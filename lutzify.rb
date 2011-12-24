a9c769769c90fa84661d47803e7ae65a71918cb6
Derek Erdmann <derek@derekerdmann.com>
2011-12-24 13:27:32 -0500

    Added two images and randomly picking one of them for the response

--- lutzify.rb
+++ lutzify.rb
@@ -11,3 +11,14 @@ end
 get "/css" do
   scss :style, style: :expanded
 end
+
+get "/img" do
+  content_type "image/jpeg"
+
+  image_dir = Dir.new( "images" )
+  image = image_dir.reject{ |f| File.directory? f }.map{ |f| f }.sample
+
+  File.open( "images/#{image}" ) do |image|
+    image.readlines
+  end
+end
